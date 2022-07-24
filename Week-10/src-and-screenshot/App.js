03545dcaf537d563d2bcfd56ba39ad6716de3084
samrat-rm <93928124+samrat-rm@users.noreply.github.com>
2022-07-24 20:19:05 +0530

    Add files via upload

---
+++ Week-10/src-and-screenshot/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import Main from "./Components/Main";
+export default function App(props) {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
+  );
+}
